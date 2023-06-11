1ef4496b1d5ca45845082e4e18a71914fc359b60
shafuk <2424834335@qq.com>
2023-06-11 15:44:41 +0800

    update

--- test1.js
+++ test1.js
@@ -2,26 +2,20 @@ import sha256 from "./crypto-js/sha256.js"
 function test(date1,date2,date3,date4,date5){
     var temp1 = []
     temp1.push(date1,date2,date3,date4,date5)
-
     fuzhi(temp1)
-
     var temp2 = []
     for (let i = 0; i < temp1.length; i++) {
         temp2.push(jiami1(temp1[i]))
     }
-
     var temp3 = []
     for (let i = 0; i < temp2.length; i+=2) {
         temp3.push(jiami2(temp2[i]+temp2[i+1]))
     }
-
     fuzhi(temp3)
-
     var temp4 = []
     for (let i = 0; i < temp3.length; i+=2) {
         temp4.push(jiami2(temp3[i]+temp3[i+1]))
     }
-
     return jiami2(temp4[0]+temp4[1])
 }
 function jiami1(x){
@@ -39,7 +33,6 @@ function fuzhi(array){
         array.push(array[array.length-1])
     }
 }
-
 function main(){
     console.log(test("123", "456", "789", "234", "764"))
 }
